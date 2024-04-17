36be676cbb96cc2fd7a99a49a3bd4c879f544c24
sttadic <stjepan.tadich@gmail.com>
2024-04-17 18:00:51 +0000

    Added functionality to a script to display the date and time in dd mm yy format and to print hostname. Used a couple of echo commands for formatting

--- script.sh
+++ script.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+echo ""
+echo "Display the current date and time"
+echo "-----------------------------------------------------"
+date | awk '{print $3, $2, $6, $4}'
+echo ""
+
+echo ""
+echo "Print system's host name"
+echo "-----------------------------------------------------"
+hostname
+echo ""
+
