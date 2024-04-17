ac11fe0a0b1a7c5b96aa5eb7b48c43d8b6b330b4
sttadic <stjepan.tadich@gmail.com>
2024-04-17 23:25:16 +0000

    Slightly modified the output of the echo commands. Script has been run and the output is written to a text file stjepanTadic.txt

--- script.sh
+++ script.sh
@@ -6,61 +6,61 @@ RESET="\033[0m"
 exec >> stjepanTadic.txt
 
 echo ""
-echo -e "${RED}***  Current Date and Time ***"
+echo -e "\t\t${RED}***  Current Date and Time ***"
 echo -e "---------------------------------------------------------------------------${RESET}"
 date | awk '{print $3, $2, $6, $4}'
 echo ""
 
 echo ""
-echo -e "${RED}***  System's Host Name  ***"
+echo -e "\t\t${RED}***  System's Host Name  ***"
 echo -e "---------------------------------------------------------------------------${RESET}"
 hostname
 echo ""
 
 echo ""
-echo -e "${RED}***  Architecture Name  ***"
+echo -e "\t\t${RED}***  Architecture Name  ***"
 echo -e "---------------------------------------------------------------------------${RESET}"
 arch
 echo ""
 
 echo ""
-echo -e "${RED}***  System Information  ***"
+echo -e "\t\t${RED}***  System Information  ***"
 echo -e "---------------------------------------------------------------------------${RESET}"
 uname -a
 echo ""
 
 echo ""
-echo -e "${RED}***  System's Uptime / Number of Users  ***"
+echo -e "\t\t${RED}***  System's Uptime / Number of Users  ***"
 echo -e "---------------------------------------------------------------------------${RESET}"
 uptime
 echo ""
 
 echo ""
-echo -e "${RED}***  Current User  ***"
+echo -e "\t\t${RED}***  Current User  ***"
 echo -e "---------------------------------------------------------------------------${RESET}"
 whoami
 echo ""
 
 echo ""
-echo -e "${RED}***  Logged In Users  ***"
+echo -e "\t\t${RED}***  Logged In Users  ***"
 echo -e "---------------------------------------------------------------------------${RESET}"
 who
 echo ""
 
 echo ""
-echo -e "${RED}***  Logged In Users and Their Processes  ***"
+echo -e "\t\t${RED}***  Logged In Users and Their Processes  ***"
 echo -e "---------------------------------------------------------------------------${RESET}"
 w
 echo ""
 
 echo ""
-echo -e "${RED}***  Currently Running System Processes  ***"
+echo -e "\t\t${RED}***  Currently Running System Processes  ***"
 echo -e "---------------------------------------------------------------------------${RESET}"
 top -b -n 1
 echo ""
 
 echo ""
-echo -e "${RED}***  History of Commands  ***"
+echo -e "\t\t${RED}***  History of Commands  ***"
 echo -e "---------------------------------------------------------------------------${RESET}"
 cat ~/.bash_history
 echo ""
