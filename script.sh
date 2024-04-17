831ae36d2ddcecdba0170ba0a00bc4a39eb28caf
sttadic <stjepan.tadich@gmail.com>
2024-04-17 18:30:48 +0000

    Additional two commands added to a script: arch and uname. Changed some formatting of echo commands for headings

--- script.sh
+++ script.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
 echo "" >> stjepanTadic.txt
-echo "Display the current date and time" >> stjepanTadic.txt
+echo "***  Current Date and Time ***" >> stjepanTadic.txt
 echo "-----------------------------------------------------" >> stjepanTadic.txt
 date | awk '{print $3, $2, $6, $4}' >> stjepanTadic.txt
 echo "" >> stjepanTadic.txt
 
 echo "" >> stjepanTadic.txt
-echo "Print system's host name" >> stjepanTadic.txt
+echo "***  System's Host Name  ***" >> stjepanTadic.txt
 echo "-----------------------------------------------------" >> stjepanTadic.txt 
 hostname >> stjepanTadic.txt
 echo "" >> stjepanTadic.txt
 
+echo "" >> stjepanTadic.txt
+echo "***  Architecture Name  ***" >> stjepanTadic.txt
+echo "-----------------------------------------------------" >> stjepanTadic.txt
+arch >> stjepanTadic.txt
+echo "" >> stjepanTadic.txt
+
+echo "" >> stjepanTadic.txt
+echo "***  System Information  ***" >> stjepanTadic.txt
+echo "-----------------------------------------------------" >> stjepanTadic.txt
+uname -a >> stjepanTadic.txt
+echo "" >> stjepanTadic.txt
