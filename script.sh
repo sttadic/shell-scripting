15353c2e817e768e9d944c1ff50851fa9a446389
sttadic <stjepan.tadich@gmail.com>
2024-04-17 18:52:24 +0000

    Removed output redirection from each line. Instead I used exec command at the beginning of script to automatically redirect output of all subsequent commands to a text file

--- script.sh
+++ script.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-echo "" >> stjepanTadic.txt
-echo "***  Current Date and Time ***" >> stjepanTadic.txt
-echo "-----------------------------------------------------" >> stjepanTadic.txt
-date | awk '{print $3, $2, $6, $4}' >> stjepanTadic.txt
-echo "" >> stjepanTadic.txt
+exec >> stjepanTadic.txt
 
-echo "" >> stjepanTadic.txt
-echo "***  System's Host Name  ***" >> stjepanTadic.txt
-echo "-----------------------------------------------------" >> stjepanTadic.txt 
-hostname >> stjepanTadic.txt
-echo "" >> stjepanTadic.txt
+echo ""
+echo "***  Current Date and Time ***"
+echo "-----------------------------------------------------"
+date | awk '{print $3, $2, $6, $4}'
+echo ""
 
-echo "" >> stjepanTadic.txt
-echo "***  Architecture Name  ***" >> stjepanTadic.txt
-echo "-----------------------------------------------------" >> stjepanTadic.txt
-arch >> stjepanTadic.txt
-echo "" >> stjepanTadic.txt
+echo ""
+echo "***  System's Host Name  ***"
+echo "-----------------------------------------------------"
+hostname
+echo ""
 
-echo "" >> stjepanTadic.txt
-echo "***  System Information  ***" >> stjepanTadic.txt
-echo "-----------------------------------------------------" >> stjepanTadic.txt
-uname -a >> stjepanTadic.txt
-echo "" >> stjepanTadic.txt
+echo ""
+echo "***  Architecture Name  ***"
+echo "-----------------------------------------------------"
+arch
+echo ""
+
+echo ""
+echo "***  System Information  ***"
+echo "-----------------------------------------------------"
+uname -a
+echo ""
