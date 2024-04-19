6cd6ca4558493125a1062e6399088e3ede52a5ab
sttadic <stjepan.tadich@gmail.com>
2024-04-19 10:55:01 +0000

    Added prompts for employee details, also did some formatting with echo

--- salScales.sh
+++ salScales.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+echo ""
+echo "********************************************"
+echo "********** EMPLOYEE SALARY SCALES **********"
+echo "********************************************"
+echo ""
+echo "Please enter the following details:"
+# Read input from a user
+read -p "  Starting Salary -> " salary
+read -p "  Number of Points -> " points
+read -p "  Salary Increment -> " increment
+read -p "  Is the Employee a Manager? (y/n) -> " isManager
